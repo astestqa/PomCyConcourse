6de0df61ac8e97defab0ccea180e87bbbf1f8c48
astestqa <tiguidou7098@gmail.com>
2025-09-18 19:42:22 -0400

    version 2025-09.4

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports', // Répertoire où les rapports seront enregistrés  
+    charts: true, // Active les graphiques dans le rapport
+    reportPageTitle: 'custom-title', // Définit un titre personnalisé pour la page du rapport
+    embeddedScreenshots: true, // Intègre les captures d'écran directement dans le rapport
+    inlineAssets: true, // Intègre les fichiers CSS et JS dans le rapport HTML
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
   },
 });
