f77717047e6f57989dc468335909d74d582ae693
astestqa <tiguidou7098@gmail.com>
2025-09-19 13:53:59 -0400

    version 2025-09.5

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,12 +8,16 @@ export default defineConfig({
     reportPageTitle: 'custom-title', // Définit un titre personnalisé pour la page du rapport
     embeddedScreenshots: true, // Intègre les captures d'écran directement dans le rapport
     inlineAssets: true, // Intègre les fichiers CSS et JS dans le rapport HTML
+    saveAllAttempts: false, // Enregistre toutes les tentatives de test, pas seulement la dernière
   },
   e2e: {
+    testIsolation: false, // Désactive l'isolation des tests pour partager l'état entre les tests
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on);
       return config;
     },
+    //defaultCommandTimeout: 10000, // Temps d'attente par défaut pour les commandes
+    //pageLoadTimeout: 60000, // Temps d'attente pour le chargement des pages
   },
 });
